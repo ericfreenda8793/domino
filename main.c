5b80e75a0771181711f3898cf13cb2ce5d75dda4
ericfreenda8793 <74149309+ericfreenda8793@users.noreply.github.com>
2020-11-30 01:37:27 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -1,20 +1,67 @@
 #include<stdio.h>
-
-int countWays(int n)
+char b[3][22];
+int count=0;
+void countWays(int startr,int startc,int n)
 {
-    int A[n + 1], B[n + 1];
-    A[0] = 1, A[1] = 0, B[0] = 0, B[1] = 1;
-    for (int i = 2; i <= n; i++) {
-        A[i] = A[i - 2] + 2 * B[i - 1];
-        B[i] = A[i - 1] + B[i - 2];
+
+    if(startc==n){
+
+        count++;
+
     }
 
-    return A[n];
+    if(b[startr][startc]=='0'){
+        //2 ways to implement
+  for(int i=0;i<2;i++){
+  if(i==0){
+
+  if(startr+1<3 && b[startr+1][startc]=='0'){
+        //vertical
+        b[startr+1][startc]=b[startr][startc]='1';
+       countWays(startr+1,startc,n);
+       b[startr+1][startc]=b[startr][startc]='0';
+
+  }
+  }
+  else if(i==1){
+    if(startc+1<n && b[startr][startc+1]=='0'){
+        //horizontal
+        b[startr][startc+1]=b[startr][startc]='1';
+        if(startr+1!=3){
+         countWays(startr+1,startc,n);
+        }
+        else{
+            countWays(0,startc+1,n);
+        }
+        b[startr][startc+1]=b[startr][startc]='0';
+
+    }
+  }
+
+
+   }
+    }else if(b[startr][startc]=='1'){
+    if(startr==2){
+       countWays(0,startc+1,n);
+    }
+    else{
+      countWays(startr+1,startc,n);
+    }
+
+    }
+
+
+
+
 }
 int main(void)
 {
     int n;
     scanf("%d",&n);
-    printf("%d\n",countWays(n));
+    for(int i=0;i<n;i++){
+        b[1][i]=b[2][i]=b[0][i]='0';
+    }
+   countWays(0,0,n);
+printf("%d\n",count);
     return 0;
 }
